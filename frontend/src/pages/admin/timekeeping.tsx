d084a1612d51372d692f58705f71af68d493f9dc
lyevan <samelmido22@gmail.com>
2025-09-08 23:58:26 +0000

    feat: enhance timekeeping page with area chart and attendance status features

--- frontend/src/pages/admin/timekeeping.tsx
+++ frontend/src/pages/admin/timekeeping.tsx
@@ -1,11 +1,50 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { formatDate } from "@/lib/stringMethods";
 import { LogIn, LogOut } from "lucide-react";
 import axios from "axios";
 import { useUserSessionStore } from "@/store/userSessionStore";
 import { toast } from "sonner";
 import { useState } from "react";
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { TrendingUp } from "lucide-react";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+
+export const description = "An area chart with a legend";
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig;
 
 const TimekeepingPage = () => {
   const { employee } = useUserSessionStore();
@@ -51,41 +90,43 @@ const TimekeepingPage = () => {
 
   return (
     <div className="space-y-6">
-      <div>
+      {/* <div>
         <h1 className="text-3xl font-bold tracking-tight">
           Timekeeping Management
         </h1>
         <p className="text-muted-foreground">
           Monitor and manage employee attendance records
         </p>
-      </div>
+      </div> */}
 
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Present Today</CardTitle>
+            <CardTitle className="text-sm font-medium">
+              Total Hours for a Week
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">142</div>
-            <p className="text-xs text-muted-foreground">
-              Out of 152 employees
-            </p>
+            <p className="text-xs text-muted-foreground">Hours</p>
           </CardContent>
         </Card>
 
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Late Arrivals</CardTitle>
+            <CardTitle className="text-sm font-medium">
+              Active Employees
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">7</div>
-            <p className="text-xs text-muted-foreground">Today</p>
+            <p className="text-xs text-muted-foreground">Currently</p>
           </CardContent>
         </Card>
 
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">On Break</CardTitle>
+            <CardTitle className="text-sm font-medium">Absentee</CardTitle>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">12</div>
@@ -95,37 +136,102 @@ const TimekeepingPage = () => {
 
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Absent</CardTitle>
+            <CardTitle className="text-sm font-medium">Median Hours</CardTitle>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">10</div>
-            <p className="text-xs text-muted-foreground">Today</p>
+            <p className="text-xs text-muted-foreground">Weekly</p>
           </CardContent>
         </Card>
 
         <Card className="col-span-2">
           <CardHeader>
-            <CardTitle>
-              Your Attendance Status Today: {formatDate(Date.now() as any)}
-            </CardTitle>
+            <CardTitle>Area Chart - Legend</CardTitle>
+            <CardDescription>
+              Showing total visitors for the last 6 months
+            </CardDescription>
           </CardHeader>
           <CardContent>
-            <div className="w-full flex justify-between">
-              <Button onClick={() => handleClockIn()} disabled={isLoading}>
-                <LogIn />
-                Clock-In
-              </Button>
-              <Button
-                variant={"destructive"}
-                onClick={() => handleClockOut()}
-                disabled={isLoading}
+            <ChartContainer config={chartConfig}>
+              <AreaChart
+                accessibilityLayer
+                data={chartData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}
               >
-                <LogOut />
-                Clock-Out
-              </Button>
-            </div>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="month"
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent indicator="line" />}
+                />
+                <Area
+                  dataKey="mobile"
+                  type="natural"
+                  fill="var(--color-mobile)"
+                  fillOpacity={0.4}
+                  stroke="var(--color-mobile)"
+                  stackId="a"
+                />
+                <Area
+                  dataKey="desktop"
+                  type="natural"
+                  fill="var(--color-desktop)"
+                  fillOpacity={0.4}
+                  stroke="var(--color-desktop)"
+                  stackId="a"
+                />
+                <ChartLegend content={<ChartLegendContent />} />
+              </AreaChart>
+            </ChartContainer>
           </CardContent>
+          <CardFooter>
+            <div className="flex w-full items-start gap-2 text-sm">
+              <div className="grid gap-2">
+                <div className="flex items-center gap-2 leading-none font-medium">
+                  Trending up by 5.2% this month{" "}
+                  <TrendingUp className="h-4 w-4" />
+                </div>
+                <div className="text-muted-foreground flex items-center gap-2 leading-none">
+                  January - June 2024
+                </div>
+              </div>
+            </div>
+          </CardFooter>
         </Card>
+        <div className="col-span-2">
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                Your Attendance Status Today: {formatDate(Date.now() as any)}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="w-full flex justify-between">
+                <Button onClick={() => handleClockIn()} disabled={isLoading}>
+                  <LogIn />
+                  Clock-In
+                </Button>
+                <Button
+                  variant={"destructive"}
+                  onClick={() => handleClockOut()}
+                  disabled={isLoading}
+                >
+                  <LogOut />
+                  Clock-Out
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
